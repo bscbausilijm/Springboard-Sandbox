67418ed6304e09f0ebd66c329ba42f543a69d538
Justin Bausili <bsc.bausilijm@gmail.com>
2026-06-22 07:16:18 -0400

    small update

--- casinoGames/script.js
+++ casinoGames/script.js
@@ -1,6 +1,31 @@
 const casinoGame = {
-
+    shoe: {
+        deck: {
+            card: {
+                makeCard: function(value, suit) {
+                    return { value, suit }; // AKA: return { value: value, suit: suit };
+                }
+            }, //end card object
+        //deck object keys
+            deckArray: [],
+            cardGrid: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
+            suitGrid: ["of Hearts", "of Spades", "of Diamonds", "of Clubs"],
+            loadDeck: function() {
+                let card;
+                this.suitGrid.forEach((suit) => {
+                    this.cardGrid.forEach((value) => {
+                        this.deckArray.push(this.card.makeCard(value, suit));
+                    });
+                });
+            }
+        }, //end deck object
+    }, //end shoe object
 };
+
+const cardSmallParse = (card) => {
+    const { value, suit } = card;
+    return value;
+}
 
 //json parse
 const jParse = (args) => {
@@ -15,7 +40,7 @@ const jStringify = (args) => {
 document.addEventListener("DOMContentLoaded", () => {
     const head = document.querySelector("#playerUIHeader");
     const header = document.createElement("h1");
-    header.textContent = "Menu:";
+    header.textContent = "test: ";
     head.appendChild(header);
 });
 
@@ -27,8 +52,13 @@ const pushCard = (n) => {
     table.appendChild(card);
 }
 
-const runCardTest = () => {
-    for (let x = 0; x <= 4; x++) pushCard("lol");
-}
+const runCardTest = (n) => {
+    let card;
+    for (let x = 0; x <= 5; x++) {
+        card = casinoGame.shoe.deck.deckArray[x];
+        pushCard(card.value);
+    }
+}   
 
+casinoGame.shoe.deck.loadDeck();
 runCardTest();
